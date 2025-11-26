295696421e16f1c92f77306f267df46ff08191df
Keerati_Lert <44543287+keeratioat@users.noreply.github.com>
2025-11-26 07:47:36 +0000

    day 2 trainning

--- part-2/Labfiles/03-create-plugins/C-sharp/Program.cs
+++ part-2/Labfiles/03-create-plugins/C-sharp/Program.cs
@@ -21,10 +21,13 @@ var kernel = builder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
 // Add the plugin to the kernel
-
+kernel.Plugins.AddFromType<FlightBookingPlugin>("FlightBookingPlugin");
 
 // Configure function choice behavior
-
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
 
 var history = new ChatHistory();
 history.AddSystemMessage("The year is 2025 and the current month is January");
